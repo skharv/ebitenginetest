aeb901e51a04690ea49a1fdb10254fcb5cb5918a
Simon <simon.k.harv@gmail.com>
2022-06-09 22:08:28 +1000

    initial commit

---
+++ character.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Character struct {
+	posX float64
+	posY float64
+	width int
+	height int
+	sprite *ebiten.Image 
+}
+
+func (c *Character) Init(x, y float64, filepath string) {
+	
+	img, _, err := ebitenutil.NewImageFromFile(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.posX = x
+	c.posY = y
+	c.sprite = img
+	c.width, c.height = c.sprite.Size()
+}
+
+func (c *Character) Update() {
+	if(inpututil.IsKeyJustPressed(ebiten.KeyA)) {
+		c.posX--
+	}
+	if(inpututil.IsKeyJustPressed(ebiten.KeyD)) {
+		c.posX++
+	}
+}
+
+func (c *Character) Draw(screen *ebiten.Image, options *ebiten.DrawImageOptions) {	
+	options.GeoM.Scale(1, 1)
+	options.GeoM.Translate(c.posX - float64(c.width / 2), c.posY - float64(c.height / 2))
+
+	screen.DrawImage(c.sprite, options)
+}
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import(
+	_ "image/png"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	screenWidth = 640
+	screenHeight = 480
+)
+
+var(
+	player Character
+)
+
+type Game struct{
+}
+
+func (g *Game) Update() error {
+	player.Update();
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	player.Draw(screen, op);
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+		return screenWidth, screenHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Isn't he cute?")
+
+	player.Init(screenWidth/2, screenHeight/2, "images/babby.png")
+
+	game := &Game{}
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
+}
